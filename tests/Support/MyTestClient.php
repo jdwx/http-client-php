ff794d27a23ebebc40e56576c789c4f988a8bde9
JDW <xs@jdw.sx>
2025-03-29 00:39:32 -0400

    MyTestClient can now throw arbitrary exceptions.

--- tests/Support/MyTestClient.php
+++ tests/Support/MyTestClient.php
@@ -7,11 +7,11 @@ declare( strict_types = 1 );
 namespace Support;
 
 
-use JDWX\HttpClient\Exceptions\NetworkException;
 use JDWX\HttpClient\Simple\SimpleResponse;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Throwable;
 
 
 class MyTestClient implements ClientInterface {
@@ -22,20 +22,25 @@ class MyTestClient implements ClientInterface {
 
 
     /**
-     * @param array<string, ResponseInterface|int|string> $rResponses Map of path => response.
+     * @param array<string, ResponseInterface|int|string|Throwable> $rResponses Map of path => response.
      *
-     * If the response is an int 0, it will throw a NetworkException. Otherwise,
-     * it will return an error response with the given status code.
+     * If the response is an int it will return an error response with the
+     * given status code.
      *
      * If the response is a string, it will be the body content of an otherwise
      * default response.
+     *
+     * If the response is a ResponseInterface, it will be returned as-is.
+     *
+     * If the response is a Throwable, it will be thrown.
      */
-    public function __construct( public array $rResponses = [] ) {}
+    public function __construct( public array $rResponses = [] ) { }
 
 
     public function error404() : ResponseInterface {
         return ( new SimpleResponse( '404 Not Found', uStatusCode: 404, stReasonPhrase: 'Not Found' ) )
-            ->withHeader( 'Content-Type', 'text/plain' );
+            ->withHeader( 'Content-Type', 'text/plain' )
+        ;
     }
 
 
@@ -52,15 +57,15 @@ class MyTestClient implements ClientInterface {
 
         $rsp = $this->rResponses[ $stPath ];
         if ( is_int( $rsp ) ) {
-            if ( 0 === $rsp ) {
-                throw new NetworkException( $request, 'Simulated network exception.' );
-            }
             return new SimpleResponse( 'Oh no error!', uStatusCode: $rsp, stReasonPhrase: 'Simulated error' );
         }
         if ( is_string( $rsp ) ) {
             return new SimpleResponse( $rsp );
         }
-        return $rsp;
+        if ( $rsp instanceof ResponseInterface ) {
+            return $rsp;
+        }
+        throw $rsp;
     }
 
 
